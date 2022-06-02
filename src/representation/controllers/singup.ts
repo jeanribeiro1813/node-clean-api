9625588837d6e32a9d9f2124913f2ccb51943fad
jeanribeiro1813 <jeanribeiro1813@gmail.com>
2022-06-02 12:14:44 -0300

    feat: ensure SingUpControllers return 400 if no email provided

--- src/representation/controllers/singup.ts
+++ src/representation/controllers/singup.ts
@@ -1,10 +1,18 @@
 export class SingUpController{
     handle(httpRequest: any): any{
-
+        if(!httpRequest.body.name){
         return {
             statusCode:400,
             body: new Error('Missing param: name')
         }
+    }
+
+    if(!httpRequest.body.email){
+        return {
+            statusCode:400,
+            body: new Error('Missing param: email')
+        }
+    }
 
     }   
 }
