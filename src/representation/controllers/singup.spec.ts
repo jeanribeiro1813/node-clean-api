d2525b378303e148617babc42f60f146c2330dfd
jeanribeiro1813 <jeanribeiro1813@gmail.com>
2022-06-06 11:44:53 -0300

    test: ensure SingUpController returns 400 if an invalid email is provided

--- src/representation/controllers/singup.spec.ts
+++ src/representation/controllers/singup.spec.ts
@@ -1,17 +1,36 @@
 import {SingUpController} from './singup'
 import {MissingParamsError} from '../errors/missing-params-error'
+import { InvalidParamsError } from '../errors/invalid-params-error';
+import { EmailValidator } from '../protocols/email-validator';
 
+interface SutTypes{
+    sut: SingUpController,
+    emailValidatorStub: EmailValidator
+}
 
-const makeSut = ():SingUpController =>{
-    return new SingUpController();
+//Para Evitar qualquer dependencia e ficar mudando um teste por um , o ideal é fazer isso para deixar o codigo mais limpo
+//Assim eu posso colocar quantas dependenciar eu quiser e não necessariamente preciso mudar em cada parte
+const makeSut = ():SutTypes =>{
+
+    class EmailValidatorStub implements EmailValidator{
+        isValid(email:string):boolean{
+            return true
+        }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut =  new SingUpController(emailValidatorStub);
+    return{
+        sut,
+        emailValidatorStub
+    }
 }
 
 describe('SingUp Controller',() =>{
     test('Should return 400 if no name is provided',()=>{
-        const sut = makeSut()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
-                email:'any_email',
+                email:'any_email@mail.com',
                 password:'any_password',
                 passwordConfirmation: 'any_password'
             }
@@ -23,7 +42,7 @@ describe('SingUp Controller',() =>{
     })
 
     test('Should return 400 if no email is provided',()=>{
-        const sut = new SingUpController()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 name:'any_name',
@@ -38,11 +57,11 @@ describe('SingUp Controller',() =>{
     })
 
     test('Should return 400 if no password is provided',()=>{
-        const sut = makeSut()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 name:'any_name',
-                email:'any_email',
+                email:'any_email@mail.com',
                 passwordConfirmation: 'any_password'
             }
         }
@@ -53,17 +72,37 @@ describe('SingUp Controller',() =>{
     })
 
     test('Should return 400 if no password confirmation is provided',()=>{
-        const sut = makeSut()
+        const {sut} = makeSut()
         const httpRequest = {
             body:{
                 name:'any_name',
-                email:'any_email',
-                password: 'any_password'
+                email:'any_email@mail.com',
+                password: 'any_password',
+
             }
         }
     const httpResponse =  sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamsError('passwordConfirmation'))
+
+    })
+
+    test('Should return 400 if an invalid email is provided',()=>{
+        const {sut, emailValidatorStub} = makeSut()
+        jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+        const httpRequest = {
+            body:{
+                name:'any_name',
+                //Dando mais semantica para o teste , assim dando mais visu do que eu estou testando
+                email:'invalid_email@mail.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+
+            }
+        }
+    const httpResponse =  sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamsError('email'))
 
     })
 })
