819765bb06b6ac7d9f04767d5917671b1d4bb527
jeanribeiro1813 <jeanribeiro1813@gmail.com>
2022-06-06 12:21:41 -0300

    feat: ensure SingUpController returns 500 if EmailValidator throws

--- src/representation/controllers/singup.ts
+++ src/representation/controllers/singup.ts
@@ -4,18 +4,22 @@ import {badRequest} from '../helpers/http-helpers'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamsError } from '../errors/invalid-params-error';
+import { ServerError } from '../errors/server-error';
 
 
 export class SingUpController implements Controller{
 
+    //Criando uma variavel private , pois só vai pode mexer dentro da classe
     private readonly emailValidator:EmailValidator
 
+    //Fazendo o constructor para injetar no teste
+    //No qual recebe o emailValidator
     constructor(emailValidator:EmailValidator){
         this.emailValidator = emailValidator
 
     }
     handle(httpRequest: HttpRequest): HttpResponse{
-    
+        try{
         const requiredFields = ['name','email','password','passwordConfirmation']
     
         for (const field of requiredFields){
@@ -24,10 +28,21 @@ export class SingUpController implements Controller{
     
         }
     }
+
+    //Pegando o email do bodu e validando se esta correto
     const isValid = this.emailValidator.isValid(httpRequest.body.email)
     if(!isValid){
+        //Erro criado para o email
         return badRequest(new InvalidParamsError('email'))
     }
 
-    }   
+    } catch(error){
+
+        return{
+            statusCode: 500,
+            body:new ServerError
+        }
+
+    }
+}   
 }
