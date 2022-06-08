d7567667fbd75e72581894b5155ab69764ffaa46
jeanribeiro1813 <jeanribeiro1813@gmail.com>
2022-06-08 19:23:32 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -3,6 +3,7 @@ import validator  from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator{
 
+    //Obrigatorio trazer esse metodo pois foi implementado da interface criada para email validator
     isValid(email:string):boolean{
         return validator.isEmail(email)
     }
