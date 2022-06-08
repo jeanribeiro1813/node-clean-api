6add0ebacd5251514748f2022f6d4dd95a8e64cc
jeanribeiro1813 <jeanribeiro1813@gmail.com>
2022-06-08 19:15:54 -0300

    chore: add jest configs and scripts

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,5 +1,6 @@
 const config = require('./jest.config')
 
+//Pegando a configuração do jest e pegando tudo que tiver a extensão .ts
 config.testMatch = ['**/*.test.ts']
 
 module.exports = config
--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,5 +1,6 @@
 const config = require('./jest.config')
 
+//Pegando a configuração do jest e pegando tudo que tiver a extensão spec.ts
 config.testMatch = ['**/*.spec.ts']
 
 module.exports = config
